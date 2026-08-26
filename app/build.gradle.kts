69068d65c6a3b4411a520bf94616b1e892b46052
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2026-08-26 10:27:05 -0300

    chore(ads): added ad id declaration in manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "digital.tonima.mycarcompanion"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
